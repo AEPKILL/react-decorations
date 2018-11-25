b372eebfa98c00619bbb61df949e3efa2c2313e4
AEPKILL <a@aepkill.com>
2018-11-25 19:01:57 +0800

    test: add test for isGetter

---
+++ src/utils/is-getter.test.ts
@@ -0,0 +1,35 @@
+import isGetter from './is-getter';
+
+describe('utils/isFunction', () => {
+  // we don't test more such as number, null and other
+  // because we use isFunction and isFunction was pass these test
+  test('not getter need return false', () => {
+    expect(isGetter({ name: 'AEPKILL' }, 'name')).toBe(false);
+    expect(
+      isGetter(
+        {
+          name() {
+            return 'AEPKILL';
+          }
+        },
+        'name'
+      )
+    ).toBe(false);
+  });
+  test(`give 'null' not to throw exception and return false`, () => {
+    expect(() => isGetter(null, 'xxxx')).not.toThrow();
+    expect(isGetter(null, 'xxx')).toBe(false);
+  });
+  test('getter need return true', () => {
+    expect(
+      isGetter(
+        {
+          get name() {
+            return 'AEPKILL';
+          }
+        },
+        'name'
+      )
+    ).toBe(true);
+  });
+});
