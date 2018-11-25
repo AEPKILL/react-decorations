f05da519708ccef4eed95d733bbffb09e434ddfc
AEPKILL <a@aepkill.com>
2018-11-25 19:15:25 +0800

    test: add test case for 100% coverage
    - utils/is-getter.ts when give a not exist property need return false.

--- src/utils/is-getter.test.ts
+++ src/utils/is-getter.test.ts
@@ -3,6 +3,7 @@ import isGetter from './is-getter';
 describe('utils/isFunction', () => {
   // we don't test more such as number, null and other
   // because we use isFunction and isFunction was pass these test
+
   test('not getter need return false', () => {
     expect(isGetter({ name: 'AEPKILL' }, 'name')).toBe(false);
     expect(
@@ -16,10 +17,16 @@ describe('utils/isFunction', () => {
       )
     ).toBe(false);
   });
+
+  test('when give a not exist property need return false', () => {
+    expect(isGetter({ name: 'AEPKILL' }, 'names' as 'name')).toBe(false);
+  });
+
   test(`give 'null' not to throw exception and return false`, () => {
     expect(() => isGetter(null, 'xxxx')).not.toThrow();
     expect(isGetter(null, 'xxx')).toBe(false);
   });
+
   test('getter need return true', () => {
     expect(
       isGetter(
