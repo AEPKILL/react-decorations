c543d339325f369acf4773f85fbcc3324d7e639f
AEPKILL <a@aepkill.com>
2018-11-25 18:31:25 +0800

    fix: give a class `isFunction` also return true
      i think isFunction(class A{}) right way is return false

--- src/utils/is-function.ts
+++ src/utils/is-function.ts
@@ -6,7 +6,24 @@ const FUNCION_TYPE_STRING = typeof isFunction;
  * @template T
  * @param {T} fnLike
  * @returns {boolean}
+ * @example
+ * class A {} // not a function
+ * function A {} // function
+ * const A = () => 2333 // function
  */
-export default function isFunction<T>(fnLike: T): boolean {
-  return typeof fnLike === FUNCION_TYPE_STRING;
+// tslint:disable-next-line:no-any
+export default function isFunction(fnLike: any): boolean {
+  if (typeof fnLike === FUNCION_TYPE_STRING) {
+    const fnString = fnLike.toString();
+    // equal with fnString.startWith('class'), but `startWith` dont work on all browser and i don't want to use polyfill.
+    // `const fn = class => class` SyntaxError
+    return !(
+      fnString[0] === 'c' &&
+      fnString[1] === 'l' &&
+      fnString[2] === 'a' &&
+      fnString[3] === 's' &&
+      fnString[4] === 's'
+    );
+  }
+  return false;
 }
